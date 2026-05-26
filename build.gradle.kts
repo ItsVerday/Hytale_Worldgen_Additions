2aca9dacf2f939fdce29708e994217c198de4e93
Verday <32226766+ItsVerday@users.noreply.github.com>
2026-05-26 12:45:17 -0600

    Add version to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
  * NOTE: This is entirely optional and basics can be done in `settings.gradle.kts`
  */
 
+rootProject.version = extra["project.version"] as String
+
 repositories {
     // Any external repositories besides: MavenLocal, MavenCentral, HytaleMaven, and CurseMaven
 }
